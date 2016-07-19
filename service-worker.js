95f3b4544c917792d72e29eb0f803f0bbf5177da
komkanit <kom.sujautra@gmail.com>
2016-07-19 20:32:12 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 importScripts('node_modules/sw-toolbox/sw-toolbox.js');
 
-toolbox.precache(['/','/index.html', '/bundle.js']);
+toolbox.precache(['./','./index.html', './bundle.js']);
 
 toolbox.options.debug = true;
 
@@ -12,6 +12,6 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-toolbox.router.get('/index.html', toolbox.fastest);
-toolbox.router.get('/', toolbox.fastest);
-toolbox.router.get('/bundle.js', toolbox.fastest);
+toolbox.router.get('./index.html', toolbox.fastest);
+toolbox.router.get('./', toolbox.fastest);
+toolbox.router.get('./bundle.js', toolbox.fastest);
